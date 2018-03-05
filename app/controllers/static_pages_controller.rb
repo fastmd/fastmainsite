4d7413c6e69e12950beddcaa7e4b22a879bfc690
lolo <jdanov1@yandex.ru>
2018-03-05 16:57:15 +0200

    v2018030503 hosting

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,4 +12,32 @@ class StaticPagesController < ApplicationController
   def contact
   end
   
+  def hosting
+    @hosting0 = Hosting.new("0", "Landing", "100 MDL", "5 EUR")
+  end  
+  
+private  
+  
+  class Hosting
+   @@no_of_hosting = 0
+   def initialize(id, name, mpricemdl, mpriceeur, ypricemdl, ypriceeur, hdd)
+      @id = id
+      @name = name
+      @mpricemdl = mpricemdl
+      @mpriceeur = mpriceeur
+      @ypriceeur = mpriceeur
+      @ypriceeur = mpriceeur
+      @hdd = hdd
+   end
+   def name
+     @name
+   end
+   def lprice
+     @lprice
+   end
+   def eprice
+     @eprice
+   end
+  end
+  
 end
