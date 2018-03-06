e25e275a36e65cf867c1cb14cf2b5ed56b8c1b7b
lolo <jdanov1@yandex.ru>
2018-03-06 16:37:38 +0200

    v2018030602

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,20 +14,20 @@ class StaticPagesController < ApplicationController
   
   def hosting
     @hosting = Array.new
-    @hosting[0] = Hosting.new("0", "LANDING", "The best offer", nil, "100 MDL", "5 EUR", "400 Mb")
-    @hosting[1] = Hosting.new("1", "MICRO", "20 MDL", "1 EUR", "200 MDL", "10 EUR", "1 Gb")
-    @hosting[2] = Hosting.new("2", "MINI", "40 MDL", "2 EUR", "280 MDL", "14 EUR", "3 Gb")
-    @hosting[3] = Hosting.new("3", "STANDARD", "60 MDL", "3 EUR", "400 MDL", "20 EUR", "6 Gb")
-    @hosting[4] = Hosting.new("4", "BUSINESS", "80 MDL", "4 EUR", "600 MDL", "30 EUR", "10 Gb")
-    @hosting[5] = Hosting.new("5", "BUSINESS LARGE", "120 MDL", "6 EUR", "1000 MDL", "50 EUR","20 Gb")
-    @hosting[6] = Hosting.new("6", "BUSINESS EXTRA", "160 MDL", "8 EUR", "1200 MDL", "60 EUR","40 Gb (+ 10 Gb: 1 € per month./ 10 € per year)")
+    @hosting[0] = Hosting.new("0", "LANDING", "The best offer", nil, "100 MDL", "5", "400 Mb")
+    @hosting[1] = Hosting.new("1", "MICRO", "20 MDL", "1", "200 MDL", "10", "1 Gb")
+    @hosting[2] = Hosting.new("2", "MINI", "40 MDL", "2", "280 MDL", "14", "3 Gb")
+    @hosting[3] = Hosting.new("3", "STANDARD", "60 MDL", "3", "400 MDL", "20", "6 Gb")
+    @hosting[4] = Hosting.new("4", "BUSINESS SIMPLE", "80 MDL", "4", "600 MDL", "30", "10 Gb")
+    @hosting[5] = Hosting.new("5", "BUSINESS LARGE", "120 MDL", "6", "1000 MDL", "50","20 Gb")
+    @hosting[6] = Hosting.new("6", "BUSINESS EXTRA", "160 MDL", "8", "1200 MDL", "60","40 Gb", "+ 10 Gb: 1€ per month / 10€ per year")
   end  
   
 private  
   
   class Hosting
    @@no_of_hosting = 0
-   def initialize(id, name, mprice_mdl, mprice_eur, yprice_mdl, yprice_eur, hdd)
+   def initialize(id, name, mprice_mdl, mprice_eur, yprice_mdl, yprice_eur, hdd, comment = nil)
       @id = id
       @name = name
       @mprice_mdl = mprice_mdl
@@ -35,6 +35,7 @@ private
       @yprice_mdl = yprice_mdl
       @yprice_eur = yprice_eur
       @hdd = hdd
+      @comment = comment
    end
    def name
      @name
@@ -53,6 +54,9 @@ private
    end
    def hdd
      @hdd
+   end
+   def comment
+     @comment
    end
   end
   
