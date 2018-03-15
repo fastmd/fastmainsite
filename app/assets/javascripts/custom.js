7384a9228054d654170b8d65ad03d6f51198691d
lolo <jdanov1@yandex.ru>
2018-03-15 16:59:11 +0200

    v2018031502 addresses

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -34,3 +34,11 @@
 			    }
 			  });
 			});
+			
+function myMap() {
+var myCenter = new google.maps.LatLng(47.0196395,28.843749);
+var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var marker = new google.maps.Marker({position:myCenter});
+marker.setMap(map);
+}
