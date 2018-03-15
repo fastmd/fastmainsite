5c53be732882936960da1c471e49cb9e57f86dd9
lolo <jdanov1@yandex.ru>
2018-03-15 11:54:02 +0200

    v20180315 small-devs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,7 +15,7 @@ class StaticPagesController < ApplicationController
   def hosting
     @hosting = Array.new
     @hosting[0] = Hosting.new("0", "LANDING", "The best offer", nil, "100 MDL", "5", "400 Mb")
-    @hosting[1] = Hosting.new("1", "MICRO", "20 MDL", "1", "200 MDL", "10", "1 Gb")
+    @hosting[1] = Hosting.new("1", "MICRO", "20 MDL", "1", "200 MDL", "10", "1 Gb", nil, "1")
     @hosting[2] = Hosting.new("2", "MINI", "40 MDL", "2", "280 MDL", "14", "3 Gb")
     @hosting[3] = Hosting.new("3", "STANDARD", "60 MDL", "3", "400 MDL", "20", "6 Gb")
     @hosting[4] = Hosting.new("4", "BUSINESS SIMPLE", "80 MDL", "4", "600 MDL", "30", "10 Gb")
@@ -27,7 +27,7 @@ private
   
   class Hosting
    @@no_of_hosting = 0
-   def initialize(id, name, mprice_mdl, mprice_eur, yprice_mdl, yprice_eur, hdd, comment = nil)
+   def initialize(id, name, mprice_mdl, mprice_eur, yprice_mdl, yprice_eur, hdd, comment = nil, flag = nil)
       @id = id
       @name = name
       @mprice_mdl = mprice_mdl
@@ -36,6 +36,7 @@ private
       @yprice_eur = yprice_eur
       @hdd = hdd
       @comment = comment
+      @flag = flag
    end
    def name
      @name
@@ -58,6 +59,9 @@ private
    def comment
      @comment
    end
+   def flag
+     @flag
+   end   
   end
   
 end
